66a34e8a3327336388262c79f069eed3e467f0b1
Sebastian Martinez <s.martinezgd@gmail.com>
2018-08-25 12:40:25 -0500

    Pequeños cambios de estilo

--- galeria/urls.py
+++ galeria/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from galeria import views
 
 urlpatterns = [
+    path('media/<int:id>', views.detail, name='media-detail'),
     path('', views.index, name='media-list'),
-    path('media/<int:id>', views.detail, name='media-detail')
 ]
