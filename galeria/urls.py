8d9962aabb62511bbeda19836a6324c6b5ed4e6b
Sebastian Martinez <s.martinezgd@gmail.com>
2018-09-02 10:18:34 -0500

    Implemente la busqueda por tipo y categoria

--- galeria/urls.py
+++ galeria/urls.py
@@ -4,4 +4,5 @@ from galeria import views
 urlpatterns = [
     path('media/<int:id>', views.detail, name='media-detail'),
     path('', views.index, name='media-list'),
+    path('search/', views.galerySearch, name='media-search'),
 ]
--- galeria/views.py
+++ galeria/views.py
@@ -1,15 +1,48 @@
 from django.shortcuts import render
 
-from .models import Media, Clip
+from .models import Media, Clip, Category
 from .forms import ClipForm
 
 # Create your views here.
 def index(request):
 
     media = Media.objects.all()[:12]
+    categories = Category.objects.all()
 
     context = {
-        'media': media
+        'searchParams': {
+            'type': 'B',
+            'category': '',
+        },
+        'media': media,
+        'categories': categories
+    }
+    return render(request, 'galeria/mediaList.html', context)
+
+def galerySearch(request):
+    mediaType = request.GET['mediaType']
+    category = request.GET['category']
+
+    media = Media.objects.all()
+    if (mediaType != 'B'):
+        media = media.filter(type=mediaType)
+    if (category != '' and int(category) >= 0):
+        cat = Category.objects.get(id=int(category))
+        media = media.filter(category=cat.id)
+
+    media = media[:12]
+    categories = Category.objects.all()
+    categoryId = category
+    if (categoryId != ''):
+        categoryId = int(categoryId)
+    context = {
+        'searchParams': {
+            'type': mediaType,
+            'category': categoryId,
+        },
+        'category': category,
+        'media': media,
+        'categories': categories
     }
     return render(request, 'galeria/mediaList.html', context)
 
